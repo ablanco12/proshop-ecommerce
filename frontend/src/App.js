86ec562e6470eb8409113e81ed40f81fe6a623a4
Andrew - Joseph Blanco <andrewblanco@andrews-mbp-3.lan>
2023-10-12 18:57:43 -0500

    Working on Rating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,19 @@
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
-
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 const App = () => {
   return (
     <>
-    <Header />
-      <main className='py-3'>
+      <Header />
+      <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
-    <Footer />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,17 @@
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+
+const Rating = ({ value, text }) => {
+  return (
+    <div className="rating">
+      <span>
+        {value >= 1 ? (
+          <FaStar />
+        ) : value >= 0.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+    </div>
+  );
+};
